ee155991b2f8e585a58ca6e151c21d63785dc56d
AbdulsalamAmtech <abdulsalamamtech@gmail.com>
2024-06-21 21:34:38 +0100

    Modified backend register.js file

--- assets/backend/register.js
+++ assets/backend/register.js
@@ -1,11 +1,14 @@
 
 
-console.log(`${app.url}/register.html`);
+console.log(`processing: ${app.url}/register.html`);
 
 $(document).ready(function() {
 
     console.log(`${app.apiUrl}/register`);
     localStorage.setItem('token', "");
+    localStorage.setItem('user', "");
+    app.log(`${app.apiUrl}/register`);
+
 
 
     $('#registerForm').on('submit', function(event) {
@@ -36,19 +39,19 @@ $(document).ready(function() {
             data: JSON.stringify(formData),
             success: function(response) {
 
-                alert('Registration successful: ' + response.responseJSON);
+                alert('Registration successful: ' + response.message);
+
                 // Assuming the response contains the user data
                 localStorage.setItem('token', JSON.stringify(response.access_token));
-                app.token = response.access_token
                 localStorage.setItem('user', JSON.stringify(response.data));
-                app.user = response.data
 
-                // window.location.href = 'dashboard.html';
+                // window.location.href = `${app.url}/dashboard.html`;
+                app.location("/dashboard.html");
 
-                console.log(response);
+                // console.log(response);
                 // console.log(response.success);
                 // console.log(response.message);
-                console.log(response.data);
+                // console.log(response.data);
                 // console.log(response.access_token);
 
                 console.log(JSON.stringify(response.data));
@@ -60,14 +63,17 @@ $(document).ready(function() {
             },
             error: function(error) {
 
-                alert('Registration failed: ' + error.responseText);
+                // alert('Registration failed: ' + error.responseText);
+                alert('Registration failed: ' + error.responseJSON.message + "\n" 
+                    + error.responseJSON.error + "\n\t\n" + JSON.stringify(error.responseJSON.errors)
+                );
 
-                console.log(error.responseJSON);
-                console.log(error.responseJSON.message); //
+                console.log(error.responseJSON.success);
+                console.log(error.responseJSON.message); 
                 console.log(error.responseJSON.error);
-                console.log(error.responseJSON.errors);
+                console.log(error.responseJSON.errors || error.responseJSON.error);
 
-                console.log(JSON.stringify(error.responseJSON.message));
+                // console.log(JSON.stringify(error.responseJSON.message));
 
             },
             complete: function() {
