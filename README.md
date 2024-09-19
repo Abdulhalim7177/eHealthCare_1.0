71f59df1fbf5260b19b7fce6d9f2bdf354a52e7b
Abdulhalim Muhammad Yaro <123118801+Abdulhalim7177@users.noreply.github.com>
2024-09-19 07:13:09 +0100

    Initial commit

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 eHealthCare is a medical software that helps to bridge the gap 
 between rural and urban health care by providing electronic 
 health care to help patients
+
+## Team Members
+* Abdulhalim Muhammad Yaro
+* Abdulsalam Abdurrahaman 
+* Fatima BAshir Garba
+* Ahmad Salis
+* Amina Muhammad
+
--- assets/js/map.js
+++ assets/js/map.js
@@ -19,7 +19,7 @@ function getCoordinates() {
 
             // Load and display tile layers on the map
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                maxZoom: 19,
+                maxZoom: 22,
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(map);
 
